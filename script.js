77985cc47a398fcfa719a6d5bc22a51d9d4c5cbd
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-05-02 16:50:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//String method problem exersize...
+const drink = '     water';
+const liquid = 'water    ';
+if(drink.trim() === liquid.trim()){
+    console.log('Pani hani dami jani');
+} else{
+    console.log('Somudre pani dekhte valo khaite kalo');
+}
